274eaa045316e350a959d0ed331fc387c9d46d87
Alexander Meinecke <alexandermeinecke@web.de>
2021-10-11 10:14:43 +0200

    grundlagen für communication arduino - webserver

---
+++ Server/src/classes/com.py
@@ -0,0 +1,9 @@
+
+
+class Com:
+    def __init__(self):
+        self.name = []
+        self.chickenID = []
+
+    def uploadCom(self):
+        print(self.name)
--- Server/src/main.py
+++ Server/src/main.py
@@ -1,5 +1,7 @@
 from flask import Flask, redirect, url_for
 from markupsafe import escape
+from classes.com import Com
+#import Com
 from classes.chicken import chicken
 
 app = Flask(__name__)
@@ -11,11 +13,24 @@ def rootIndex():
 
 @app.route('/api')
 def apiIndex():
+    #newCom = com(1)
     return 'OK', 200
 
 @app.route('/chicken/<int:c_number>')
 def show_chicken_info(c_number):
+    print('TEST')
     return 'You choose chicken number: {0}'.format(escape(c_number))
+
+@app.route('/communication')
+def submitData():
+    #testcode - hat eigentlich kein sinn
+    newCom = Com()
+    newCom.name = "11.10.2021-11:49"
+    newCom.test = 3
+    newCom.chickenID = [1]
+    newCom.uploadCom()
+    return 'transmitted'
+
 
 @app.errorhandler(404)
 def page_not_found(error):
@@ -24,3 +39,4 @@ def page_not_found(error):
 # ensure interpreter assigns __name__ variable
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
+
