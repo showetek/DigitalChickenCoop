cf3cdd5b355a5da59806b600e5958c95cccdd93b
Torben Heine <t.heine02@hotmail.com>
2021-10-12 11:13:01 +0200

    structure, errorhandler(method, JSON response)

--- Server/src/main.py
+++ Server/src/main.py
@@ -1,10 +1,15 @@
-from flask import Flask, redirect, url_for
+#lib import
+from logging import error
+from flask import Flask, redirect, url_for, request, jsonify
 from markupsafe import escape
+#class import
 from classes.dataset import dataSet
-#import Com
 from classes.chicken import chicken
 
+#Create basic app
 app = Flask(__name__)
+
+"""             ROUTING             """
 
 @app.route('/')
 def rootIndex():
@@ -13,12 +18,12 @@ def rootIndex():
 
 @app.route('/api')
 def apiIndex():
-    #newCom = com(1)
     return 'OK', 200
+
+
 
 @app.route('/chicken/<int:c_number>')
 def show_chicken_info(c_number):
-    print('TEST')
     return 'You choose chicken number: {0}'.format(escape(c_number))
 
 @app.route('/action/<int:id>')
@@ -29,10 +34,24 @@ def createDataSet():
 
     return 'transmitted'
 
+"""             Errorhandler             """
 
 @app.errorhandler(404)
-def page_not_found(error):
-    return '404: Page not found', 404
+def ressource_not_found(error):
+    return jsonify({
+        "status": "error",
+        "code": "404",
+        "message": "Ressource not found",
+    }), 404
+
+
+@app.errorhandler(405)
+def method_not_allowed(error):
+    return jsonify({
+        "status": "error",
+        "code": "405",
+        "message": "Methode not allowed",
+    }), 405
 
 # ensure interpreter assigns __name__ variable
 if __name__ == '__main__':
