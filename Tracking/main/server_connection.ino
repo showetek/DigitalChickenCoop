901de97dc03e6c4feeec495e243d0f2f295ee61f
geraldroboom <gerald.roboom@gmail.com>
2021-10-12 12:18:45 +0200

    Arduino UNO works, but ESP module cannot initialize

--- Tracking/main/server_connection.ino
+++ Tracking/main/server_connection.ino
@@ -1,129 +1,106 @@
 /*
-  Repeating Wifi Web Client
- This sketch connects to a a web server and makes a request
- using an Arduino Wifi shield.
+ WiFiEsp example: ConnectWPA
  
- Circuit:
- * WiFi shield attached to pins SPI pins and pin 7
- created 23 April 2012
- modified 31 May 2012
- by Tom Igoe
- 
- modified 13 Jan 2014
- by Federico Vanzati
-
- http://www.arduino.cc/en/Tutorial/WifiWebClientRepeating
- This code is in the public domain.
+ This example connects to an encrypted WiFi network using an ESP8266 module.
+ Then it prints the  MAC address of the WiFi shield, the IP address obtained
+ and other network details.
+ For more details see: http://yaab-arduino.blogspot.com/p/wifiesp-example-connect.html
 */
 
-#include <SPI.h>
-#include <WiFi.h>
+/*
+12:14:47.294 -> [WiFiEsp] Initializing ESP module
+12:14:48.290 -> [WiFiEsp] >>> TIMEOUT >>>
+12:14:50.279 -> [WiFiEsp] >>> TIMEOUT >>>
+12:14:52.273 -> [WiFiEsp] >>> TIMEOUT >>>
+12:14:54.276 -> [WiFiEsp] >>> TIMEOUT >>>
+12:14:56.320 -> [WiFiEsp] >>> TIMEOUT >>>
+12:14:57.281 -> [WiFiEsp] Cannot initialize ESP module
+12:15:03.313 -> [WiFiEsp] >>> TIMEOUT >>>
+12:15:03.313 -> [WiFiEsp] No tag found
+12:15:03.313 -> WiFi shield not present
+*/
 
-char ssid[] = "yourNetwork";      //  your network SSID (name)
-char pass[] = "secretPassword";   // your network password
-int keyIndex = 0;            // your network key Index number (needed only for WEP)
+#include "WiFiEsp.h"
 
-int status = WL_IDLE_STATUS;
+// Emulate Serial1 on pins 6/7 if not present
+#ifndef HAVE_HWSERIAL1
+#include "SoftwareSerial.h"
+SoftwareSerial Serial1(6, 7); // RX, TX
+#endif
 
-// Initialize the Wifi client library
-WiFiClient client;
+char ssid[] = "Chicken_Intranet";            // your network SSID (name)
+char pass[] = "1234567890";        // your network password
+int status = WL_IDLE_STATUS;     // the Wifi radio's status
 
-// server address:
-char server[] = "www.arduino.cc";
-//IPAddress server(64,131,82,241);
+void setup()
+{
+  // initialize serial for debugging
+  Serial.begin(115200);
+  // initialize serial for ESP module
+  Serial1.begin(9600);
+  // initialize ESP module
+  WiFi.init(&Serial1);
 
-unsigned long lastConnectionTime = 0;            // last time you connected to the server, in milliseconds
-const unsigned long postingInterval = 10L * 1000L; // delay between updates, in milliseconds
-
-void setup() {
-  //Initialize serial and wait for port to open:
-  Serial.begin(9600);
-  while (!Serial) {;} // wait for serial port to connect. Needed for native USB port only
-
-  // check for the presence of the shield:
+  // check for the presence of the shield
   if (WiFi.status() == WL_NO_SHIELD) {
     Serial.println("WiFi shield not present");
-
-    // don't continue:
+    // don't continue
     while (true);
   }
-  
-  String fv = WiFi.firmwareVersion();
 
-  if (fv != "1.1.0") {
-    Serial.println("Please upgrade the firmware");
-  }
-  
-  // attempt to connect to Wifi network:
-  while (status != WL_CONNECTED) {
-    Serial.print("Attempting to connect to SSID: ");
+  // attempt to connect to WiFi network
+  while ( status != WL_CONNECTED) {
+    Serial.print("Attempting to connect to WPA SSID: ");
     Serial.println(ssid);
-
-    // Connect to WPA/WPA2 network. Change this line if using open or WEP network:
+    // Connect to WPA/WPA2 network
     status = WiFi.begin(ssid, pass);
-
-    // wait 10 seconds for connection:
-    delay(10000);
   }
-  // you're connected now, so print out the status:
-  printWifiStatus();
+
+  Serial.println("You're connected to the network");
 }
 
-void loop() {
-  // if there's incoming data from the net connection.
-  // send it out the serial port.  This is for debugging
-
-  // purposes only:
-  while (client.available()) {
-    char c = client.read();
-    Serial.write(c);
-  }
-  // if ten seconds have passed since your last connection,
-  // then connect again and send data:
-  if (millis() - lastConnectionTime > postingInterval) {
-    httpRequest();
-  }
+void loop()
+{
+  // print the network connection information every 10 seconds
+  Serial.println();
+  printCurrentNet();
+  printWifiData();
+  
+  delay(10000);
 }
 
-// this method makes a HTTP connection to the server:
-void httpRequest() {
-  // close any connection before send a new request.
-  // This will free the socket on the WiFi shield
-  client.stop();
-
-  // if there's a successful connection:
-  if (client.connect(server, 80)) {
-    Serial.println("connecting...");
-
-    // send the HTTP PUT request:
-    client.println("GET /latest.txt HTTP/1.1");
-    client.println("Host: www.arduino.cc");
-    client.println("User-Agent: ArduinoWiFi/1.1");
-    client.println("Connection: close");
-    client.println();
-
-    // note the time that the connection was made:
-    lastConnectionTime = millis();
-  } else {
-    // if you couldn't make a connection:
-    Serial.println("connection failed");
-  }
-}
-
-void printWifiStatus() {
-
-  // print the SSID of the network you're attached to:
-  Serial.print("SSID: ");
-  Serial.println(WiFi.SSID());
-
-  // print your WiFi shield's IP address:
+void printWifiData()
+{
+  // print your WiFi shield's IP address
   IPAddress ip = WiFi.localIP();
   Serial.print("IP Address: ");
   Serial.println(ip);
 
-  // print the received signal strength:
+  // print your MAC address
+  byte mac[6];
+  WiFi.macAddress(mac);
+  char buf[20];
+  sprintf(buf, "%02X:%02X:%02X:%02X:%02X:%02X", mac[5], mac[4], mac[3], mac[2], mac[1], mac[0]);
+  Serial.print("MAC address: ");
+  Serial.println(buf);
+}
+
+void printCurrentNet()
+{
+  // print the SSID of the network you're attached to
+  Serial.print("SSID: ");
+  Serial.println(WiFi.SSID());
+
+  // print the MAC address of the router you're attached to
+  byte bssid[6];
+  WiFi.BSSID(bssid);
+  char buf[20];
+  sprintf(buf, "%02X:%02X:%02X:%02X:%02X:%02X", bssid[5], bssid[4], bssid[3], bssid[2], bssid[1], bssid[0]);
+  Serial.print("BSSID: ");
+  Serial.println(buf);
+
+  // print the received signal strength
   long rssi = WiFi.RSSI();
-  Serial.print("signal strength (RSSI):");
-  Serial.print(rssi);
-  Serial.println(" dBm");
+  Serial.print("Signal strength (RSSI): ");
+  Serial.println(rssi);
 }
