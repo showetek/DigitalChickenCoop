992b5e34bf127f9a74b595163930fd3abb6bbfda
Torben Heine <51490472+showetek@users.noreply.github.com>
2021-10-13 12:36:46 +0200

    routing reset

--- Server/src/main.py
+++ Server/src/main.py
@@ -2,9 +2,6 @@
 from flask import Flask, redirect, url_for, jsonify, abort, request
 from markupsafe import escape
 from datetime import datetime
-from pprint import pprint
-
-from werkzeug.datastructures import Range
 #class import
 from classes.sqlConnect import dataSet
 from classes.chicken import chicken
@@ -13,9 +10,20 @@ from classes.device import device, deviceManager
 #Create basic app
 app = Flask(__name__)
 
-#List of connected devices
-device_list = []
+#Create deviceManger for list of connected devices
+dM = deviceManager()
 
+# @app.before_first_request
+# def register():
+#     if dM.device_by_ip(request.remote_addr):
+#         return jsonify({
+#                     "status": "succes",
+#                     "code": "200",
+#                     "message": "Device allready registered",
+#                 }), 200
+#     else:
+#         return redirect(url_for('login'), 302)
+    
 """             ROUTING             """
 
 @app.route('/')
@@ -25,27 +33,47 @@ def rootIndex():
 
 @app.route('/api')
 def apiIndex():
-    return 'OK', 200
+    return jsonify({
+        "status": "OK",
+        "code": "200",
+        "message": "Hühnerstall-API",
+        "login": url_for('login'),
+        "sensor_endpoint": url_for('sensor'),
+        "door_endpoint": url_for('door'),
+        "food_endpoint": url_for('food')
+    }), 200
+
+@app.route('/api/door', methods=['POST'])
+def door():
+    if request.method == 'POST':
+        pass
+    else:
+        abort(405)
+
+@app.route('/api/food', methods=['POST'])
+def food():
+    if request.method == 'POST':
+        pass
+    else:
+        abort(405)
 
 @app.route('/api/login', methods=['POST'])
 def login():
     if request.method == 'POST':
-        if len(device_list) == 0:
-            device_list.append(device(ip=request.form['ip'], id = request.form['id']))
-            print('Added first')
+        tmp_device = device(ip=request.form['ip'], id = request.form['id'])
+        if len(dM.devices) == 0:
+            dM.add_device(tmp_device)
+            return tmp_device.to_json(), 200
         else:
-            for device_obj in device_list:
-                if (device_obj.ip != request.form['ip']):
-                    device_list.append(device(ip=request.form['ip'], id = 'test'))
-                    print('Added')
-                else:
-                    print('not Added')
-        #pprint(device_list[0].ip)
-        device_1 = device(ip=request.form['ip'], id = 'test')
-        return jsonify({
-            "ip": device_1.ip,
-            "id": device_1.id,
-        }), 200
+            if dM.is_inside(tmp_device):
+                return jsonify({
+                    "status": "succes",
+                    "code": "200",
+                    "message": "Device allready registered",
+                }), 200
+            else:
+                dM.add_device(tmp_device)
+                return tmp_device.to_json(), 200
     else:
         abort(405)
 
@@ -70,7 +98,6 @@ def ressource_not_found(error):
         "message": "Ressource not found",
     }), 404
 
-
 @app.errorhandler(405)
 def method_not_allowed(error):
     return jsonify({
@@ -82,4 +109,3 @@ def method_not_allowed(error):
 # ensure interpreter assigns __name__ variable
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
-
